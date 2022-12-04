3593f32e7a4c1707c3329093cb964215e05bc595
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2022-12-04 22:22:02 +0300

    Completed Lab

---
+++ index.js
@@ -0,0 +1,100 @@
+
+const chai = ('chai');
+const spies = ('chai-spies');
+chai(spices);
+
+const spy = chai.spy();
+
+function receivesAFunction(spy){
+    return (spy());
+}
+receivesAFunction (function(){return spy});
+
+
+var fn;
+function returnsANamedFunction(fn) {
+    return returnsANamedFunction;
+}
+
+var fn;
+function returnsAnAnonymousFunction(){
+    return function(){
+        console.log("returns an anonymous function");
+    }
+}
+/*function returnsAnAnonymousFunction(fn){
+    var fn;
+ console.log(fn(),"returns an anonymous function");
+}
+returnsAnAnonymousFunction(function(){
+    return "return an anonymous function";
+})
+
+//WORKING ONE ERROR - TYPEERROR FN NOT A FUNCTION
+/*
+function returnsAnAnonymousFunction(){
+var fn;
+console.log(fn(function(){"returns an anonymous function"}));
+}
+*/
+
+
+
+
+
+
+
+
+
+
+/*
+function returnsAnAnonymousFunction(){
+    // var fn = function()
+     return returnsAnAnonymousFunction;
+     }*/
+ 
+//"anonymus function");
+
+
+
+/*
+    if fn = function (){
+        return fn()
+    } else
+
+return function()
+};
+//returnsANamedFunction(fn());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* var fn;
+function returnsANamedFunction((fn = returnsANamedFunction)){
+    
+    return (fn()); 
+}*/
+
+/*function returnsANamedFunction (){
+var fn = function(){
+   
+    return returnsANamedFunction();
+}};
+returnsANamedFunction(fn);
+
+//return returnsANamedFunction();*/
